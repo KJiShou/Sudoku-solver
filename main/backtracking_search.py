c16b3e85c9779549a32ce760f46adaf4ae01b04e
HongJunChea <cheahongjun@gmail.com>
2025-04-13 01:34:31 +0800

    backtracking v2.0 update for unsolvable msg

--- main/backtracking_search.py
+++ main/backtracking_search.py
@@ -72,24 +72,30 @@ def backtrack(board, process):
 
 if __name__ == "__main__":
     sudoku_data = [
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        [5, 1, 6, 8, 4, 9, 7, 3, 2],
+        [3, 0, 7, 6, 0, 5, 0, 0, 0],
+        [8, 0, 9, 7, 0, 0, 0, 6, 5],
+        [1, 3, 5, 0, 6, 0, 9, 0, 7],
+        [4, 7, 2, 5, 9, 1, 0, 0, 6],
+        [9, 6, 8, 3, 7, 0, 5, 0, 0],
+        [2, 5, 3, 1, 8, 6, 0, 7, 4],
+        [6, 8, 4, 2, 0, 7, 0, 5, 0],
+        [7, 9, 1, 0, 5, 0, 6, 0, 8]
     ]
+
     process = []
     tracemalloc.start()
     start_time = time.time()
-    backtrack(sudoku_data, process)
+    solved = backtrack(sudoku_data, process)
     end_time = time.time()
     _, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
-    print("\n✅ Sudoku Solved!")
-    print(f"📊 Memory Usage: {peak / (1024 * 1024):.2f} MB")
-    print(f"📊 Time Usage  : {end_time - start_time:.6f} seconds\n")
-    print_sudoku(sudoku_data)
+
+    if solved:
+        print("\n✅ Sudoku Solved!")
+        print(f"📊 Memory Usage: {peak / (1024 * 1024):.2f} MB")
+        print(f"📊 Time Usage  : {end_time - start_time:.6f} seconds\n")
+        print_sudoku(sudoku_data)
+        print("\nSudoke done solving\n")
+    else:
+        print("\nThis sudoku puzzle is unsolvable \n")
